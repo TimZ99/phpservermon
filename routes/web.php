377b0724923b81bcf2c007d684f98618c3bfcc57
Tim Zandbergen <TimZ99@users.noreply.github.com>
2025-01-30 18:34:37 +0100

    Added nav to server index page and fixed routename
    Signed-off-by: Tim Zandbergen <TimZ99@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/servers', [ServerController::class, 'index'])->name('servers.index');
-    Route::get('/server/{server}', [ServerController::class, 'show'])->name('servers.show');
+    Route::get('/servers', [ServerController::class, 'index'])->name('server.index');
+    Route::get('/server/{server}', [ServerController::class, 'show'])->name('server.show');
 });
 
 require __DIR__.'/auth.php';
