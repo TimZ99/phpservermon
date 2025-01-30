dbc86c60842e8d47866584fee4b4dc2fbd26a6fd
Tim Zandbergen <TimZ99@users.noreply.github.com>
2025-01-30 20:57:33 +0100

    Added <meta>
    Signed-off-by: Tim Zandbergen <TimZ99@users.noreply.github.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,21 @@
 
         <title>{{ config('app.name', 'PHPServerMonitor') }}</title>
 
+        <meta name="description" content="PHP Server Monitor">
+        <meta name="robots" content="noindex" />
+        <!--<link rel="manifest" href="./manifest.json">-->
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <meta name="apple-mobile-web-app-title" content="PSM">
+        <link rel="apple-touch-icon" href="./phpservermon.png">
+        <meta name="msapplication-TileImage" content="./phpservermon.png">
+        <meta name="msapplication-TileColor" content="#424242">
+
+        <meta name="theme-color" content="#424242">
+        <link rel="icon" type="image/x-icon" href="./favicon.ico" />
+        <link rel="icon" type="image/png" href="./favicon.png" />
+        <link rel="apple-touch-icon" href="./favicon.png" />
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
