a8afbc84914193e1c93950ea081cb3b4ca4273e7
Tim Zandbergen <TimZ99@users.noreply.github.com>
2025-02-03 20:35:46 +0100

    Added authorization for server views
    Simple admin acces to edit or destroy server.
    Also check for suspended account, deny access to all server pages.
    
    Signed-off-by: Tim Zandbergen <TimZ99@users.noreply.github.com>

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Server\StoreServerRequest;
 use App\Http\Requests\Server\UpdateServerRequest;
+use Illuminate\Support\Facades\Gate;
 use App\Models\Server;
 
 class ServerController extends Controller
@@ -31,6 +32,7 @@ class ServerController extends Controller
      */
     public function edit(Server $server)
     {
+        Gate::authorize('admin-only');
         return view('server.edit', [
             'server' => Server::find($server->server_id)
         ]);
@@ -41,6 +43,8 @@ class ServerController extends Controller
      */
     public function update(UpdateServerRequest $request, Server $server)
     {
+        Gate::authorize('admin-only');
+
         $server->fill($request->validated());
         $server->save();
         return to_route('server.edit', $server->server_id)->with('status', 'server-updated');
@@ -51,6 +55,8 @@ class ServerController extends Controller
      */
     public function destroy(Server $server)
     {
+        Gate::authorize('admin-only');
+
         $server->delete();
         return to_route('server.index');
     }
