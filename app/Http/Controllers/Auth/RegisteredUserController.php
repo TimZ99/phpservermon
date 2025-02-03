5db7a945190d7a795cb202281488a1b56765706b
Tim Zandbergen <TimZ99@users.noreply.github.com>
2025-02-03 03:10:44 +0100

    Added admin and suspended fields
    Signed-off-by: Tim Zandbergen <TimZ99@users.noreply.github.com>

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Log;
 
 class RegisteredUserController extends Controller
 {
@@ -33,6 +34,9 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'phone' => ['string', 'max:15', 'nullable'],
+            'suspended' => ['boolean'],
+            'admin' => ['boolean'],
         ]);
 
         $user = User::create([
@@ -41,6 +45,7 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        Log::info('New user registered with id:'. $user->id);
         event(new Registered($user));
 
         Auth::login($user);
--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,8 @@ class User extends Authenticatable
         'email',
         'phone',
         'password',
+        'admin',
+        'suspended'
     ];
 
     /**
@@ -44,6 +46,8 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'admin' => 'boolean',
+            'suspended' => 'boolean',
         ];
     }
 }
