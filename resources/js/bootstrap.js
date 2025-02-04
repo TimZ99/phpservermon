e010df306281d7524ee893fa275a693eb4ba664f
Tim Zandbergen <TimZ99@users.noreply.github.com>
2025-02-04 06:04:36 +0100

    Added bootstrap to app and server index
    Signed-off-by: Tim Zandbergen <TimZ99@users.noreply.github.com>

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
-window.axios = axios;
+import * as Popper from '@popperjs/core'
+window.Popper = Popper
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+import 'bootstrap'
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,25 +27,59 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.scss', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
-
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand ps-2" href="#">PHPServerMonitor</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link @if(Route::currentRouteName() == 'dashboard')active @endif" aria-current="page" href="{{ route('dashboard') }}">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link @if(Route::currentRouteName() == 'server.index')active @endif" aria-current="page" href="{{ route('server.index') }}">Servers</a>
+                    </li>
+                </ul>
+                <li class="nav-item dropdown d-flex pe-4">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Profile
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="{{ route('profile.edit') }}">Profile</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <form method="POST" action="{{ route('logout') }}" >
+                            @csrf
+                            <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">{{ __('Log Out') }}</a></li>
+                        </form> 
+                    </ul>
+                    </li>
+                </div>
+            </div>
+        </nav>
+        <main role="main" class="container-fluid px-4">
             <!-- Page Heading -->
             @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
+                <header>
+                    {{ $header }}
                 </header>
             @endisset
-
-            <!-- Page Content -->
-            <main class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $slot }}
-            </main>
-        </div>
+            {{ $slot }}
+        </main>
+        <footer class="footer fixed-bottom" role="contentinfo">
+            <div class="container">
+                <span class="text-muted">
+                    Powered by
+                    <a href="https://github.com/phpservermon/phpservermon/" target="_blank" rel="noopener">
+                        PHPServerMonitor.
+                    </a>
+                    Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                </span>
+            </div>
+        </footer>
     </body>
 </html>
