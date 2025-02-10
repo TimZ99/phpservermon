db55ff5809f2a717c1e00ca3e16b96d6cdad9c6d
Tim Zandbergen <TimZ99@users.noreply.github.com>
2025-02-10 20:46:44 +0100

    Removed duplicate TelescopeServiceProvider
    Signed-off-by: Tim Zandbergen <TimZ99@users.noreply.github.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,6 @@ class AppServiceProvider extends \Illuminate\Support\ServiceProvider
     {
         if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
             $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
-            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
         }
     }
 
