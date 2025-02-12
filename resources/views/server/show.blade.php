3a48963bef8a90783f91289485d50467b941806b
Tim Zandbergen <TimZ99@users.noreply.github.com>
2025-02-12 02:23:20 +0100

    Fixed routes from update to edit
    Signed-off-by: Tim Zandbergen <TimZ99@users.noreply.github.com>

--- resources/views/server/show.blade.php
+++ resources/views/server/show.blade.php
@@ -7,7 +7,7 @@
             Server details
         </div>
         <div class="card-body">
-            <a href="{{route('server.update', $server->id)}}">
+            <a href="{{route('server.edit', $server->id)}}">
                 <button class="btn btn-secondary mb-4">
                     {{ __('Edit server') }}
                 </button>
