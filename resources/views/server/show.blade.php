2fbf3a2989f033f0ff3640216f5c57979608dce5
Tim Zandbergen <TimZ99@users.noreply.github.com>
2025-02-18 13:28:28 +0100

    Show checks grouped by batch id
    Signed-off-by: Tim Zandbergen <TimZ99@users.noreply.github.com>

--- resources/views/server/show.blade.php
+++ resources/views/server/show.blade.php
@@ -22,8 +22,28 @@
             <b>Name</b> {{ $server->name }}<br>
             <b>IP</b> {{ $server->ip }}<br>
             <b>Port</b> {{ $server->port }}<br>
-            @isset($server->users) <b>Users</b> @foreach($server->users as $user) {{ $user->name }}, @endforeach <br>@endisset
-            <b>Ran checks</b><br> @foreach($server->check_histories as $check) {{ $check->Name }} - {{ $check->message }}<br> @endforeach<br>
+            @isset($server->users) 
+                <b>Users</b> 
+                @foreach($server->users as $user) 
+                    {{ $user->name }}, 
+                @endforeach 
+                <br>
+            @endisset
+
+            <b>Ran checks</b><br> 
+            @php
+                $previousBatchId = null;
+            @endphp
+            @foreach($server->check_histories->sortBy(['batch_id', 'created_at']) as $check)
+                @if($previousBatchId !== null && $previousBatchId !== $check->batch_id)
+                    <br>
+                @endif
+                {{ $check->Name }} - {{ $check->message }}<br>
+                @php
+                    $previousBatchId = $check->batch_id;
+                @endphp
+            @endforeach
+            <br>
             <b>Created at</b> {{ $server->created_at }}<br>
             <b>Updated at</b> {{ $server->updated_at }}<br>
             <b>Check settings</b>{{ $server->check_settings }}<br>
