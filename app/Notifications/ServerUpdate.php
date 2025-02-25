7a089b6898a214b9392a2ff7416a69f25cbc4795
Tim Zandbergen <TimZ99@users.noreply.github.com>
2025-02-25 04:12:49 +0100

    Ran pint
    Signed-off-by: Tim Zandbergen <TimZ99@users.noreply.github.com>

--- app/Notifications/ServerUpdate.php
+++ app/Notifications/ServerUpdate.php
@@ -1,15 +1,17 @@
 <?php
+
 namespace App\Notifications;
 
-use NotificationChannels\Telegram\TelegramMessage;
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notification;
-use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Log;
+use NotificationChannels\Telegram\TelegramMessage;
 
 class ServerUpdate extends Notification implements ShouldQueue
 {
     use Queueable;
-    
+
     public function __construct()
     {
         $this->onQueue('notifications');
@@ -17,18 +19,26 @@ class ServerUpdate extends Notification implements ShouldQueue
 
     public function via($notifiable)
     {
-        return ["telegram"];
+        return ['telegram'];
     }
 
     public function toTelegram($notifiable)
     {
-        if (!$notifiable->telegram_user_id) {
-            return;
+        if (is_null($notifiable->telegram_user_id)) {
+            Log::error('No Telegram ID found for user.', [$notifiable]);
+
+            return null;
         }
-        return TelegramMessage::create()
+        Log::info('Sending Telegram notification.', [$notifiable->telegram_user_id]);
+
+        return TelegramMessage::create('Your server has been updated successfully!')
             ->to($notifiable->telegram_user_id)
-            ->content("Your server has been updated.")
-            ->button('View Server', 'https://example.com/server');
+            ->onError(function ($data) {
+                Log::error('Failed to send Telegram notification', [
+                    'chat_id' => $data['to'],
+                    'error' => isset($data['exception']) ? $data['exception']->getMessage() : 'Unknown error',
+                ]);
+            });
     }
 
     /**
