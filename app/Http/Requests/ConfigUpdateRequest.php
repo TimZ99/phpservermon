aab220c5c91b9d083799f50c7717869303ca0881
Tim Zandbergen <TimZ99@users.noreply.github.com>
2025-03-12 01:16:04 +0100

    Fixed validation
    Signed-off-by: Tim Zandbergen <TimZ99@users.noreply.github.com>

--- app/Http/Requests/ConfigUpdateRequest.php
+++ app/Http/Requests/ConfigUpdateRequest.php
@@ -15,11 +15,10 @@ class ConfigUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            // The server name must be provided and should be a string with a maximum length of 255 characters
             'locale' => ['required', 'string', 'min:2', 'max:30'],
             'timezone' => ['required', 'timezone:all', 'max:255'],
             'from_name' => ['nullable', 'string', 'max:255'],
-            'from_address' => ['nullable', 'email:rcf,filter_unicode', 'max:255'],
+            'from_address' => ['nullable', 'email:rfc,filter_unicode', 'max:255'],
             'telegram_bot_token' => ['nullable', 'string', 'min:20', 'max:50'],
         ];
     }
